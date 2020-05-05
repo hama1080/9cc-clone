2951934de769e2551142a4d4a65af90be9ae64b7
hama1080 <12245259+hama1080@users.noreply.github.com>
2020-05-05 16:21:47 +0900

    エラー箇所を報告するerror_atを追加

--- 9cc.c
+++ 9cc.c
@@ -33,6 +33,22 @@ void error(char *fmt, ...) {
     exit(1);
 }
 
+// 入力プログラム
+char *user_input;
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 // 次のトークンが期待している記号のときは、トークンを1つ読みすすめて真を返す。それ以外のときは偽を返す
 bool consume(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op) return false;
@@ -42,13 +58,13 @@ bool consume(char op) {
 
 // 次のトークンが期待している記号のときは、トークンを1つよみすすめる。それ以外のときはエラーを報告する
 void expect(char op) {
-    if (token->kind != TK_RESERVED || token->str[0] != op) error("'%c'ではありません", op);
+    if (token->kind != TK_RESERVED || token->str[0] != op) error_at(token->str, "'%c'ではありません", op);
     token = token->next;
 }
 
 // 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。それ以外のときはエラーを報告する
 int expect_number() {
-    if (token->kind != TK_NUM) error("数ではありません");
+    if (token->kind != TK_NUM) error_at(token->str, "数ではありません");
     int val = token->val;
     token = token->next;
     return val;
@@ -66,7 +82,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 }
 
 // 入力文字列pをトークナイズしてそれを返す
-Token *tokenize(char *p) {
+Token *tokenize() {
+    char *p = user_input;
     Token head;
     head.next = NULL;
     Token *cur = &head;
@@ -88,7 +105,7 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(p, "トークナイズできません");
     }
     new_token(TK_EOF, cur, p);
     return head.next;
@@ -100,8 +117,9 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    user_input = argv[1];
     // トークナイズする
-    token = tokenize(argv[1]);
+    token = tokenize();
 
     // アセンブリの前半部分
     printf(".intel_syntax noprefix\n");
